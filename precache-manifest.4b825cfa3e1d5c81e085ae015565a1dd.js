fe6161b32316be29e6eba1fd0152a486c87cf1cb
Alex Cabrera <alex.cabrera@gmail.com>
2019-08-29 18:33:17 -0400

    Updates

---
+++ precache-manifest.4b825cfa3e1d5c81e085ae015565a1dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78b9dba4b0382fd2e7cca2bd0420dd4",
+    "url": "/FairVis/index.html"
+  },
+  {
+    "revision": "9fd9e7554189c20b61fb",
+    "url": "/FairVis/static/css/main.9fa74223.chunk.css"
+  },
+  {
+    "revision": "defd2a1c95b1c92f3215",
+    "url": "/FairVis/static/js/2.db4de688.chunk.js"
+  },
+  {
+    "revision": "9fd9e7554189c20b61fb",
+    "url": "/FairVis/static/js/main.e8abaeda.chunk.js"
+  },
+  {
+    "revision": "34730e01619f251436ec",
+    "url": "/FairVis/static/js/runtime~main.15f444ac.js"
+  },
+  {
+    "revision": "f0b3247488188947f37539a736279a8b",
+    "url": "/FairVis/static/media/adult.f0b32474.csv"
+  },
+  {
+    "revision": "6aa63d4ae56cf25a0b70f23d08618617",
+    "url": "/FairVis/static/media/compas.6aa63d4a.csv"
+  }
+]);
